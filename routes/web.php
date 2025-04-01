ecb5d02e184996b786188afa97834fe84007afca
danielxdrj20 <carlosinocencio.feb@gmail.com>
2025-04-01 11:23:21 -0300

    estudo de rotas

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,15 @@ Route::get('/', function () {
     return view('index');
 });
 
+Route::get('/mentoria', function(){
+    return 'ola';
+});
+
+
+Route::get('/teste/{id}', function() {
+    return 'certo';
+});
+
+Route::get('/teste/{token_usuario}', function() {
+    return 'certo';
+});
